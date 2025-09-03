2247ed32a941044783c4de366912ca0f38215a87
bestfriendai <120160378+bestfriendai@users.noreply.github.com>
2025-09-03 16:32:25 -0400

    chore: remove unused import in FunctionsConfig

--- lib/config/functions_config.dart
+++ lib/config/functions_config.dart
@@ -1,4 +1,3 @@
-import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform, TargetPlatform;
 import '../firebase_options.dart';
 import 'app_config.dart';
